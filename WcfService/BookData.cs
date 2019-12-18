31b28d4e3fee3acd7f4fb1f318e987877064344f
SentDyem <sentdyem@my.com>
2019-12-18 18:47:34 +0300

    Revert "Revert "Fixed bugs in WCF service""
    This reverts commit 15173e0a7675d8eac879f121bd8c48f74afe4ab3.

---
+++ WcfService/BookData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfService
+{
+    public class BookData
+    {
+        [DataContract]
+        public class Contact
+        {
+            [DataMember]
+            public string Id { get; set; }
+
+            [DataMember(IsRequired = true, Name = "ФИО")]
+            public string Name { get; set; }
+
+            [DataMember(IsRequired = true, Name = "Электронная почта")]
+            [EmailAddress(ErrorMessage = "Некорректный адрес электронной почты")]
+            public string Email { get; set; }
+        }
+    }
+}
